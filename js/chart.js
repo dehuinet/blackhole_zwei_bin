9787ed89ec83f28e9e980218ac43519381e7a2e9
nagae-memooff <169192@gmail.com>
2015-02-11 15:48:29 +0800

    更新

--- js/chart.js
+++ js/chart.js
@@ -1,62 +1,67 @@
 jQuery(function($) {
 	Chart = {
         area: function (elem,data,beginTime,pointInterval,title,sec_title,format){
-            
-           
-        elem.highcharts({
+              
+                elem.highcharts({
 
-        credits: {
-          enabled: false
-        },
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: title
-        },
-        subtitle: {
-            text: sec_title
-        },
-        xAxis: {
-            type: 'datetime',
-            //minRange: 360000, // fourteen days
-            labels:{
-                formatter:function(){
-                    return Highcharts.dateFormat(format,this.value);
-                }
-                
-            }
-        },
-        yAxis: {
-            title: {
-                text: '访问次数'
-            },
-            min: 0
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
+                    credits: {
+                      enabled: false
+                    },
+                    chart: {
+                        zoomType: 'x'
+                    },
+                    title: {
+                        text: title
+                    },
+                    subtitle: {
+                        text: sec_title
+                    },
+                    xAxis: {
+                        type: 'datetime',
+                        //minRange: 360000, // fourteen days
+                        labels:{
+                            formatter:function(){
+                                return Highcharts.dateFormat(format,this.value);
+                            }
+                            
+                        },
+                        rotation: 45,
+                        align: 'right',
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: '访问次数'
+                        },
+                        min: 0
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    plotOptions: {
+                        area: {
+                            fillColor: {
+                                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                                stops: [
+                                    [0, Highcharts.getOptions().colors[0]],
+                                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                                ]
+                            },
+                            marker: {
+                                radius: 2
+                            },
+                            lineWidth: 1,
+                            states: {
+                                hover: {
+                                    lineWidth: 1
+                                }
+                            },
+                            threshold: null
+                        }
+                    },
 
         // tooltip:{
         //     // formatter: function (){
@@ -65,15 +70,241 @@ jQuery(function($) {
         //     xDateFormat: '%Y-%m-%d %H:%m'
         // },
 
-        series: [{
-            type: 'area',
-            name: '次数',
-            pointInterval: pointInterval,
-            pointStart: beginTime,
-            data: data
-        }]
-    });
+                    series: [{
+                        type: 'area',
+                        name: '次数',
+                        pointInterval: pointInterval,
+                        pointStart: beginTime,
+                        data: data
+                    }]
+                });
 
+            },
+        
+        //多线图
+        lines:function (elem,data,categories,title,sec_title,format){
+            x = null;
+            if(categories==null){
+                x = {
+                        type: 'datetime',
+                        //minRange: 360000, // fourteen days
+                        labels:{
+                            formatter:function(){
+                                return Highcharts.dateFormat(format,this.value);
+                            }
+                            
+                        }
+                    }
+            }else{
+                x= {
+                    categories:categories
+                }
+            };
+
+                elem.highcharts({
+                    credits: {
+                      enabled: false
+                    },
+                    title: {
+                        text: title,
+                        x: -20 //center
+                    },
+                    subtitle: {
+                        text: sec_title,
+                        x: -20
+                    },
+
+                    chart: {
+                        zoomType: 'x'
+                    },
+                    xAxis: x,
+                    yAxis: {
+                        title: {
+                            text: '次数'
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }],
+                        min: 0
+                    },
+                    tooltip: {
+                        valueSuffix: '次'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+            
+                    series: data
+                });
+        },//lines end
+
+        columns: function(data,title,sub_title){
+            var colors = Highcharts.getOptions().colors,
+            categories = new Array();//['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
+            for(i=0;i<data.length;i++){
+                categories.push(data[i].name);
+            }
+            name = '设备';
+                
+
+                var chart = $("#chart-container").highcharts({
+                    credits: {
+                      enabled: false
+                    },
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: title
+                    },
+                    subtitle: {
+                        text: sub_title
+                    },
+                    xAxis: {
+                        categories: categories
+                    },
+                    yAxis: {
+                        title: {
+                            text: '数量'
+                        },
+                        min: 0
+                    },
+                    plotOptions: {
+                        column: {
+                            cursor: 'pointer',
+                            point: {
+                                events: {
+                                    click: function() {
+                                        
+                                        var drilldown = this.drilldown;
+                                        if (drilldown) { // drill down
+                                            setChart(chart,drilldown.name, drilldown.categories, drilldown.data, drilldown.color);
+                                        } else { // restore
+                                            setChart(chart,name, categories, data);
+                                        }
+                                    }
+                                }
+                            },
+                            dataLabels: {
+                                enabled: true,
+                                color: colors[0],
+                                style: {
+                                    fontWeight: 'bold'
+                                },
+                                formatter: function() {
+                                    return this.y;
+                                }
+                            }
+                        }
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            var point = this.point,
+                                s = this.x +':<b>'+ this.y +'</b><br>';
+                            if (point.drilldown) {
+                                s += '点击进入 '+ point.category +' 版本统计';
+                            } else {
+                                s += '点击返回设备统计';
+                            }
+                            return s;
+                        }
+                    },
+                    series: [{
+                        name: name,
+                        data: data,
+                        color: 'white'
+                    }],
+                    exporting: {
+                        enabled: false
+                    }
+                }).highcharts(); // return chart
+            },//columns end
+
+            pie: function(data){
+                var colors = Highcharts.getOptions().colors;
+                var categories = new Array();//['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
+                for(i=0;i<data.length;i++){
+                    categories.push(data[i].name);
+                }
+                
+                name = 'Browser brands';
+                
+
+                var chart = $('#chart-container').highcharts({
+                    credits: {
+                      enabled: false
+                    },
+                    chart: {
+                        type: 'pie'
+                    },
+                    title: {
+                        text: 'Browser market share, April, 2011'
+                    },
+                    subtitle: {
+                        text: 'Click the columns to view versions. Click again to view brands.'
+                    },
+                    plotOptions: {
+                        pie: {
+                            cursor: 'pointer',
+                            point: {
+                                events: {
+                                    click: function() {
+                                        
+                                        var drilldown = this.drilldown;
+                                        if (drilldown) { // drill down
+                                            pie_data = new Array();
+                                            for(i=0;i<drilldown.categories.length;i++){
+                                                pie_data.push(new Array(drilldown.categories[i],drilldown.data[i]));
+                                            }
+                                            setChart(chart,drilldown.name, drilldown.categories, pie_data, drilldown.color);
+                                        } else { // restore
+                                            setChart(chart,name, categories, data);
+                                        }
+                                    }
+                                }
+                            },
+                            dataLabels: {
+                                enabled: true,
+                                color: colors[0],
+                                style: {
+                                    fontWeight: 'bold'
+                                },
+                                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                            }
+                        }
+                    },
+                    tooltip: {
+                        //pointFormat: '{point.name}: <b>{point.percentage:.1f}%</b>'
+                        formatter: function(){
+                            return '<b>'+this.point.name+":</b>"+this.point.percentage.toFixed(1)+'%';
+                        }
+                    },
+                    series: [{
+                        type:'pie',
+                        name: name,
+                        data: data,
+                        color: 'white'
+                    }],
+                    exporting: {
+                        enabled: false
+                    }
+                }).highcharts(); // return chart
+            }//pie end
         }
-    }
-})
+});
+
+function setChart(chart,name, categories, data, color) {
+    chart.xAxis[0].setCategories(categories, false);
+    chart.series[0].remove(false);
+    chart.addSeries({
+                    name: name,
+                    data: data,
+                    color: color || 'white'
+                    }, false);
+    chart.redraw();
+};
---
+++ js/chartArea.js
@@ -0,0 +1,277 @@
+jQuery(function($) {
+
+  ChartArea = {
+
+    datetimePicker: function(datetimeElems,interval){
+      var options = {
+        format: "yyyy-mm-dd",
+        minView: 2,
+        language:  'zh-CN',
+        autoclose: 1,
+        todayHighlight: 1,
+        minuteStep: 1
+      };
+      datetimeElems.datetimepicker(options);
+
+      interval.off('change').on('change',function(e){
+        datetimeElems.val('');
+        var selected = interval.find('option:selected').val();
+       
+        datetimeElems.datetimepicker('remove');
+        if(selected=='by_minutes'){
+          options['format'] = "yyyy-mm-dd hh:ii";
+          options['minView'] = 0;
+          options['startView'] = 2;
+        }else if (selected=='by_hours'){
+          options['format'] = "yyyy-mm-dd hh:00";
+          options['minView'] = 1;
+          options['startView'] = 2;
+        }else if (selected=='by_days' || selected=='by_weeks'){
+          options['format'] = "yyyy-mm-dd";
+          options['minView'] = 2;
+          options['startView'] = 2;
+        }else{
+          options['format'] = "yyyy-mm";
+          options['minView'] = 3;
+          options['startView'] = 3;
+        }
+        datetimeElems.datetimepicker(options);
+      });
+    },
+
+    chartArea: function(url,title,t_type){
+      
+
+    $("#search").off('click').on('click',function(ce){
+      se = vaild();
+      if(se == null){
+        return;
+      }
+      s = se[0];
+      e = se[1];
+      var interval = $("#interval").val();
+      Ajax.get(url,{'interval':interval,'start_time':s,'end_time':e},function(e1){
+        var json = JSON.parse(e1);
+        debugger;
+        if(json.status == 'ok'){
+          ts = convert_time_format(json.start_at);
+          if(t_type=='area'){
+            Chart.area ($('#chart-container'),json.data,ts[0],json.interval*1000,title,'',ts[1]);
+          }else{
+            Chart.lines ($('#chart-container'),json.data,ts[0],json.interval*1000,title,'',ts[1]);
+          }
+        }else{
+          alert(e1);
+        }
+      });
+    });
+   // $("#search").click();
+    },
+
+    chartColumns: function(url,title,sub_title){
+      Ajax.get(url,null,function(e1){
+        var json = JSON.parse(e1);
+        
+        //var json = [{"Name":"ios","Count":"5","Data":{"Name":"ios","Categories":["7.1.1","8.0","8.1","8.1.3"],"Datas":["1","2","1","1"]}},{"Name":"android","Count":"20","Data":{"Name":"android","Categories":["4.0.3","4.1.2","4.2.1","4.2.2","4.3","4.4.4"],"Datas":["1","4","3","9","2","1"]}}];
+        var colors = Highcharts.getOptions().colors;
+        datas = new Array();
+        for(i=0;i<json.length;i++){
+          name = json[i].Name;
+          y = parseInt(json[i].Count);
+          color = colors[i];
+          dd = json[i].Data;
+          drilldown = new Array();
+          drilldown.name = name+' version';
+          drilldown.categories = dd.Categories;
+          var d = new Array();
+          for(j = 0;j<dd.Datas.length;j++){
+            d.push(parseInt(dd.Datas[j]));
+          }
+          drilldown.data = d;
+          drilldown.color = color;
+          data = new Object();
+          data.y = y;
+          data.name = name;
+          data.color = color;
+          data.drilldown = drilldown;
+          datas.push(data);
+        }
+
+        Chart.columns(datas,title,sub_title);
+      });
+    },
+
+    chartLines: function(url,title,sub_title){
+      $("#search").off('click').on('click',function(ce){
+      se = vaild();
+      if(se == null){
+        return;
+      }
+      s = se[0];
+      e = se[1];
+      var interval = $("#interval").val();
+      Ajax.get(url,{'interval':interval,'start_time':s,'end_time':e},function(e1){
+        var json = JSON.parse(e1);
+        
+        msg_data = json.msg_data;
+        cmsg_data = json.cmsg_data;
+        var data = new Array();
+        
+        msg = {
+          'type': 'line',
+          'name': '工作圈',
+          'data': msg_data
+        };
+        cmsg = {
+          'type': 'line',
+          'name': '即时通讯',
+          'data': cmsg_data
+        }
+
+        data.push(msg);
+        data.push(cmsg);
+        if(json.status == 'ok'){
+          ts = convert_time_format(json.start_at);
+          categories = null;
+          if(json.interval == 86400){
+            //tian
+            msg.pointInterval = json.interval*1000;
+            msg.pointStart = convert_time_format(json.start_at)[0];
+            cmsg.pointInterval = json.interval*1000;
+            cmsg.pointStart = convert_time_format(json.start_at)[0];
+          }else if (json.interval == 86400*7){
+            //zhou
+            var weeks = new Array();
+            for(i=0;i<msg_data.length;i++){
+              weeks.push('第'+(i+json.first_week)+'周');
+            }
+            categories = weeks;
+          }else{
+            //yue
+            var months = new Array();
+            for(i=0;i<msg_data.length;i++){
+              months.push(offsetYM(json.start_at,i));
+            }
+            categories = months;
+          }
+          Chart.lines ($('#chart-container'),data,categories,title,sub_title,ts[1]);
+          
+        }else{
+          alert(e1);
+        }
+      });
+    });
+    },
+
+    full_screen: function(){
+      $("#full_screen").off('click').on('click',function(e){
+    
+            if($("#main-content").parent()[0].id=='main-article'){
+              $("#fullscreen").modal('show');
+              $("#fullscreen").append($("#main-content"));
+              $("#full_screen").text('退出全屏');
+            }else{
+              $("#fullscreen").modal('hide');
+              $("#main-article").append($("#main-content"));
+              $("#full_screen").text('全屏');
+            }
+            //Chart.columns($("#chart-container"),data);
+            if(($("#start_date")==undefined || $("#start_date").val()!='')&&($("#end_date")==undefined || $("#end_date").val()!='')){
+              $("#search").click();
+            }
+            
+            e.preventDefault();
+        });
+    }
+
+  }
+
+  
+
+    
+});
+//计算形如 1501 样式的年月组合 返回一个数组 记录从ym开始 偏移offset的年月 样式仍然是形如1501
+function offsetYM(ym,offset){
+  debugger;
+  y = ym.substr(0,2);
+  m = ym.substr(2,2);
+  m = parseInt(m)+offset;
+  y = parseInt(parseInt(y) + m/12);
+  m%=12;
+  return y+''+(m<10? 0+''+m:m);
+}
+
+//校验时间选择是不是正确 顺便返回一个数组 只有2个元素 开始时间戳 结束时间戳
+function vaild(){
+  var stime = $("#start_date").val();
+      var etime = $("#end_date").val();
+      if(stime=='' || etime == ''){
+        alert('时间范围不能为空！');
+        return null;
+      }
+      
+      var selected = $("#interval").find('option:selected').val();
+      if(selected=='by_months'){
+        stime+='-01';
+        end = etime.split('-');
+        etime = end[0]+'-'+(parseInt(end[1])+1)+"-01";
+      }
+      var s = datetime_to_unix(stime);
+      var e = datetime_to_unix(etime);
+      if (s>e){
+        alert('开始时间不能大于结束时间！');
+        return null;
+      }
+      
+      if(selected=='by_minutes'){
+        e+=60;
+      }else if (selected=='by_hours'){
+        e+=3600;
+      }else if(selected =='by_days'){
+        e+=86400;
+      }else if(selected == 'by_weeks'){
+        e+=(86400*7)
+      }else{
+        //按月
+
+      }
+      return new Array(s,e);
+}
+
+//把形如 20150205的日志转为时间戳 还有时间格式
+function convert_time_format(time){
+  var arr = new Array(5);
+  var ff = new Array(5);
+  ff[0] = '%y';
+  ff[1] = '%m';
+  ff[2] = '%d';
+  ff[3] = ' %H';
+  ff[4] = '%M';
+  format = '';
+  for(i=0;i<5;i++){
+    if (i*2 < time.length){
+      format+=ff[i];
+      arr[i] = time.substr(i*2,2);
+      if (i==0){
+        arr[i]=20+arr[i];
+      }else if (i==1){
+        arr[i]--;
+      }
+    }else{
+      arr[i] = 0;
+    }
+            
+  }
+  arr1 = new Array();
+  arr1.push(Date.UTC(arr[0],arr[1],arr[2],arr[3],arr[4]));
+  arr1.push(format);
+  return arr1;
+};
+
+//该方法把格式： 年-月-日 时:分:秒 的日期转为时间戳 年-月-日 必须有 时:分:秒 可选 年必须4位 月日时分秒 1或2位
+function datetime_to_unix(day){  
+  // alert(new Date(day).getTime());
+  //   re = /(\d{4})(?:-(\d{1,2})(?:-(\d{1,2}))?)?(?:\s+(\d{1,2}):(\d{1,2}):(\d{1,2}))?/.exec(day);  
+  //   return new Date(re[1],(re[2]||1)-1,re[3]||1,re[4]||0,re[5]||0,re[6]||0).getTime()/1000;  
+  return new Date(day).getTime()/1000;
+}
